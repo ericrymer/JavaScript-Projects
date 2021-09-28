ebe05f402930f971e99c7bca0a9c89b0dc04e0cb
ericrymer <eric.rymer@gmail.com>
2021-09-28 13:41:15 -0700

    First Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,25 @@
+alert("Click OK to view my family list!"); //This opens an alert window 
+document.write("There are a total of");//Writes out line "There are a total of"
+document.write("<br>");//this gives a break in the line
+document.write(2+2); //Calculates total of family members
+document.write("<br>");//this gives a break in the line
+document.write("of us"); //writes out line "of us"
+document.write("<br>");//this gives a break in the line
+var Family ="The Rymers", Dad ="Mickey", Mom ="Peggy", Sister ="Andrea", Brother="Eric";//This lists my family 
+var Family=Family.fontcolor("blue"); //This changes the font to "Blue"
+document.write(Family);// This writes out the value "Family"
+document.write("<br>");//this gives a break in the line
+var Dad=Dad.fontcolor("red")//This changes the font to "Red"
+document.write(Dad); // This writes out the value "Dad"
+document.write("<br>");//this gives a break in the line
+var Mom=Mom.fontcolor("orange");//This changes the font to "Orange"
+document.write(Mom); // This writes out the value "Mom"
+document.write("<br>");//this gives a break in the line
+var Sister=Sister.fontcolor("green");//This changes the font to "Green"
+document.write(Sister);// This writes out the value "Sister"
+document.write("<br>");//this gives a break in the line
+var Sister=Sister.fontcolor("black");//This changes the font to "Black"
+document.write(Brother);// This writes out the value "Brother"
+document.write("<br>");//this gives a break in the line
+var B =("They may be a lot" +" but I still love them!"); //this is an expression adding two values
+document.write(B)//this writes out the line above.
