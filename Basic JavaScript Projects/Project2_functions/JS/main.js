92e81b8b0e0d76d5767308f2da7c9fa84580253d
ericrymer <eric.rymer@gmail.com>
2021-09-28 14:16:06 -0700

    Second Project using +=Operator

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() { //Defining a function and naming it.
+    var sentence ="I am learning";//Definiing variable and giving it a string value
+    sentence+=" alot about JavaScript!"; //Definiing variable and giving it a string value
+    document.getElementById("Concatenate").innerHTML=sentence;//Putting the value of result into HTML element with plain text.
+  }
